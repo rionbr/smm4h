37aa795fd9922cfded11354e4f601fb53dc0709e
diegompin <2016bestworld>
2019-04-18 21:44:09 -0700

    - Implementing Plotter

--- src/plotter.py
+++ src/plotter.py
@@ -1,53 +1,8 @@
-__author__ = 'diegopinheiro'
-__email__ = 'diegompin@gmail.com'
-__github__ = 'https://github.com/diegompin'
 
 
 import matplotlib.pyplot as plt
-import numpy as np
 
-class Plotter(object):
-
-    def __init__(self):
-        pass
-
-
-    # TODO
-    def plot(self, scores):
-        from statsmodels.distributions.empirical_distribution import ECDF
-        fig, ax = plt.subplots()
-        ecdf = ECDF(scores)
-        bins = np.linspace(0, 1)
-        ax.plot(bins, ecdf(bins))
-        plt.show()
-
-    def bootstrap_distribution(self, scores):
-        x = scores
-        n = len(scores)
-        reps = 1000
-        xb = np.random.choice(x, (n, reps))
-        mb = xb.mean(axis=0)
-        mb.sort()
-        p_25, p_975 = np.percentile(mb, [2.5, 97.5])
-        return mb, p_25, p_975
-
-    def plot_bootstrap(self, mb):
-        fig, ax = plt.subplots()
-        ax.hist(mb)
-        plt.show()
-
-
-import pickle
-filepath = 'datalink/task1/random_forest_strategy.pickle'
-with open(filepath, 'rb') as file:
-    # file = open(self.filepath, 'rb')
-    data = pickle.load(file)
-    results = data.cv_results_
-
-
-import matplotlib.pyplot as plt
-fig
-model = 'Random Forest '
+model = 'randomforest'
 plt.figure(figsize=(13, 13))
 plt.title(f"{model}", fontsize=16)
 
@@ -56,20 +11,6 @@ plt.ylabel("Score")
 
 import maracatu as m
 
-
-def get_par_base():
-    par_base = {
-        'xvar': 'X',
-        'yvar': 'Y',
-        'title': 'Titanium Patients',
-        'xlabel': 'Date',
-
-        'alpha': 1,
-        'grid': True,
-        'fig.tight_layout': True,
-        'fig.figsize': (20, 10),
-    }
-    return par_base
 
 def plot_hist(mb, xlabel, ylabel, filepath, filename):
     par_base = get_par_base()
@@ -89,7 +30,48 @@ def plot_hist(mb, xlabel, ylabel, filepath, filename):
 
     plt.show()
 
-scorer = 'f1_score'
-for sample, style in (('train', '--'), ('test', '-')):
-    sample_score_mean = results['mean_%s_%s' % (sample, scorer)]
+def get_values(results, scorer, sample, kfold):
+    values = [results[f'split{i}_{sample}_{scorer}'] for i in range(0, kfold)]
+    return np.ravel(values)
 
+
+
+
+kfold = 10
+sample = 'test' #train, test
+scorer = 'f1_score'
+
+values = get_values(results, scorer, sample, kfold)
+
+plot_hist(values, 'F1_score (X)', 'P(X)', path_plots, f'{model}_{sample}_{scorer}.pdf')
+
+X_axis = np.array(results['param_classifier__min_samples_split'].data, dtype=float)
+
+
+fig, ax = plt.subplots(1, 1)
+
+color = 'g'
+for sample, style in (('train', '--'), ('test', '-')):
+    sample_score_mean = path_results['mean_%s_%s' % (sample, scorer)]
+    sample_score_std = path_results['std_%s_%s' % (sample, scorer)]
+    ax.fill_between(X_axis, sample_score_mean - sample_score_std,
+                    sample_score_mean + sample_score_std,
+                    alpha=0.1 if sample == 'test' else 0, color=color)
+    ax.plot(X_axis, sample_score_mean, style, color=color,
+            alpha=1 if sample == 'test' else 0.7,
+            label="%s (%s)" % (scorer, sample))
+
+    best_index = np.nonzero(path_results['rank_test_%s' % scorer] == 1)[0][0]
+    best_score = path_results['mean_test_%s' % scorer][best_index]
+
+    # Plot a dotted vertical line at the best score for that scorer marked by x
+    ax.plot([X_axis[best_index], ] * 2, [0, best_score],
+            linestyle='-.', color=color, marker='x', markeredgewidth=3, ms=8)
+
+    # Annotate the best score for that scorer
+    ax.annotate("%0.2f" % best_score,
+                (X_axis[best_index], best_score + 0.005))
+
+plt.legend(loc="best")
+plt.grid('off')
+plt.savefig(f'{path_plots}/{model}.pdf')
